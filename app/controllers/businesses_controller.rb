bf9685c70ef745c2f5dfcb47255822c77b45f8f4
eygis <ahill033@yahoo.com>
2022-09-09 18:21:16 +0900

    added form for business new method, and create method

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,8 +1,19 @@
 class BusinessesController < ApplicationController
+
+  before_action :require_login
+
   def new
+    @business = Business.new
   end
 
   def create
+    @business = Business.new(business_params)
+    if @business.save
+      redirect_to business_path(@business)
+    else
+      flash[:error] = 'invalid'
+      redirect_to new_business_path
+    end
   end
 
   def show
@@ -10,5 +21,18 @@ class BusinessesController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def require_login
+    unless user_signed_in?
+      flash[:error] = 'You must be logged in to do that.'
+      redirect_to root_path
+    end
+  end
+
+  def business_params
+    params.require(:business).permit(:business_name, :business_city, :business_votes, :business_rating)
   end
 end
--- app/models/business.rb
+++ app/models/business.rb
@@ -1,2 +1,4 @@
 class Business < ApplicationRecord
+    validates :business_name, uniqueness: true, presence: true
+    validates :business_city, presence: true
 end
