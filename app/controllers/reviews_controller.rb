3973458cb2cff8fed0df2a09384f4ee6ffc28d13
eygis <ahill033@yahoo.com>
2022-09-16 18:08:31 +0900

    review calculations appear to function correctly

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,11 +7,19 @@ class ReviewsController < ApplicationController
 
     def create
         @review = Review.new(review_params)
+        @business_id = review_params[:business_id]
         if @review.save
-            redirect_to business_path(review_params[:business_id])
+            @business = Business.find_by(id: @business_id)
+            if @business[:business_votes] != 0
+                @business.update(business_rating: rating_calculator)
+            else
+                @business.update(business_rating: review_params[:rating])
+            end
+            Business.increment_counter(:business_votes, @business_id)
+            redirect_to business_path(@business_id)
         else
             flash[:error] = 'invalid'
-            redirect_to business_path(review_params[:business_id])
+            redirect_to business_path(@business_id)
         end
     end
 
@@ -19,5 +27,9 @@ class ReviewsController < ApplicationController
 
     def review_params
         params.require(:review).permit(:review, :reviewer, :rating, :business_id)
+    end
+
+    def rating_calculator
+        ((@business[:business_rating] * @business[:business_votes]) + review_params[:rating].to_i) / (@business[:business_votes] + 1)
     end
 end
